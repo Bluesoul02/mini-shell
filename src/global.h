acf0916a98675a4fd540dc7d34844dbe879f36e9
kevin_avot <kevin_avot@ens.univ-artois.fr>
2021-10-06 11:08:40 +0200

    removing ; in global.h

--- src/global.h
+++ src/global.h
@@ -11,9 +11,9 @@
 #define LGCMD_SIZE 4096 
 #define BUFFER_SIZE 1024
 
-#define clear() printf("\e[1;1H\e[2J");
-#define syserror(x) perror(perror[errormsg[x]]);
-#define fatalsyserror(x) syserror(x), exit (x);
-#define err(m,n) printf("Error %c",m), exit(n);
-#define ROUGE(m) "\033[01;31m"m"\033[0m";
-#define VERT(m) "\033[01;32m"m"\033[0m";
+#define clear() printf("\e[1;1H\e[2J")
+#define syserror(x) perror(errormsg[x])
+#define fatalsyserror(x) syserror(x), exit (x)
+#define err(m,n) printf("Error %c",m), exit(n)
+#define ROUGE(m) "\033[01;31m"m"\033[0m"
+#define VERT(m) "\033[01;32m"m"\033[0m"
