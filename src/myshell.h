8dba48da54ebb247dd808ad1c1049eaaa103e5cf
Avot Kevin <kevin_avot@ens.univ-artois.fr>
2021-10-06 10:39:34 +0200

    ajout var globales dans myshell.h

--- src/myshell.h
+++ src/myshell.h
@@ -3,12 +3,20 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#define clear() printf("\e[1;1H\e[2J");
-#define syserr(m,n) perror(#m), exit(n);
-#define err(m,n) printf("Error %c",m), exit(n);
+#include <ctype.h>
+#include <errno.h>
+
 #define ERR -1
-#define ERR_EXEC 127
+#define FAILED_EXEC 127
+#define LGCMD_SIZE 4096 
 #define BUFFER_SIZE 1024
+
+#define clear() printf("\e[1;1H\e[2J");
+#define syserror(x) perror(perror[errormsg[x]]);
+#define fatalsyserror(x) syserror(x), exit (x);
+#define err(m,n) printf("Error %c",m), exit(n);
+#define ROUGE(m) "\033[01;31m"m"\033[0m"
+#define VERT(m) "\033[01;32m"m"\033[0m"
 
 void init();
 void printDirectory();
