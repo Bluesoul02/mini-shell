c53b991bfd57864f00e64a22126aec3d700ae684
gregoire_delacroix <gregoire_delacroix@ens.univ-artois.fr>
2021-12-21 00:18:54 +0100

    V1: &&, ||

--- src/myshell.c
+++ src/myshell.c
@@ -70,10 +70,15 @@ int requiredLine() {
         in=0;
         out=0;
         if(!fgets(lgcmd,LGCMD_SIZE-1,stdin)) break;
-        for(s=lgcmd;isspace(*s) && *s != ';';s++); // we skip the spaces and semicolons at the beginning of the sentence
+        for(s=lgcmd;isspace(*s);s++); // we skip the spaces at the beginning of the sentence
         for(i=0;*s;i++) { // for all the sentence
             tabcmd2[out][in++]=s; // we affect the actual s that represents our position in the sentence
-            while(!isspace(*s) && *s != ';') s++; // we go to the end of the actual word
+            while(!isspace(*s) && *s != ';' && (strncmp("&& ",s,3)) && (strncmp("|| ",s,3))) s++; // we go to the end of the actual word
+            if((strncmp("&& ",s,3) == 0) || (strncmp("|| ",s,3) == 0)) {
+                tabcmd2[out++][in]=NULL; // one command per array
+                in=0;
+                s+=2;
+            }
             if(*s == ';') { // multiple commands
                 tabcmd2[out++][in]=NULL; // one command per array
                 in=0;
@@ -85,10 +90,20 @@ int requiredLine() {
         if(i){
             if(in) tabcmd2[out][in]=NULL;
             for(j=0;j<=out;j++) { // one processus per task/command
+                int pos=0, and=0, or=0;
+                while(tabcmd2[j][pos] != NULL) pos++;
+                if(strcmp("&&",tabcmd2[j][--pos]) == 0) {
+                    and = 1;
+                    tabcmd2[j][pos] = NULL;
+                } else if (strcmp("||",tabcmd2[j][pos]) == 0) {
+                    or = 1;
+                    tabcmd2[j][pos] = NULL;
+                }
                 if((pid=fork()) == ERR) fatalsyserror(1);
                 if(!pid) { // execute the next command
                     for (int k = 0; k < CUSTOMCMD_SIZE; k++) {
                         if (strcmp(*tabcmd2[j], customcmd[k]) == 0) {
+                            printf("C'est myls");
                             for (int m = 1; m < in; m++) {
                                 printf("param : %s\n", tabcmd2[j][m]);
                                 if (!strncmp(tabcmd2[j][m], "-", 1)) {
@@ -114,11 +129,15 @@ int requiredLine() {
                             printf(VERT("exit status of ["));
                             for(ps=tabcmd2[j];*ps;ps++) printf("%s",*ps);
                             printf(VERT("]=%d\n"),status);
+                            if(or) break;
+                        } else {
+                            if(and) break;
                         }
                     } else puts(ROUGE("Anormal exit"));
                 }
             }
         }
+        printDirectory();
     }
     /* detach shared memory segment */  
     shmdt(shm);
